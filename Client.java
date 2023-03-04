4eac09d916c1c31628d97dfdf5ee2be5cb5f84d6
Ming Lei <31576265+popoway@users.noreply.github.com>
2023-03-04 16:22:18 -0500

    update cli parameters

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+
 /**
  * Client
  */
@@ -8,11 +9,19 @@ import java.net.Socket;
 
 public class Client {
 
-  // Connect to the TCP server at localhost:3721
+  // Connect to the TCP server at <address>:<port>
 
   public static void main(String[] args) throws Exception {
-    Socket socket = new Socket("localhost", 3721);
-    System.out.println("Connected to server");
+    if (args.length != 2) {
+      System.out.println("Usage: java Client <address> <port>");
+      return;
+    }
+    String address = args[0];
+    int port = Integer.parseInt(args[1]);
+
+    // Connect to the server
+    Socket socket = new Socket(address, port);
+    System.out.println("Connected to server " + socket.getInetAddress() + ":" + socket.getPort());
 
     // Get the input and output streams
     InputStream is = socket.getInputStream();
